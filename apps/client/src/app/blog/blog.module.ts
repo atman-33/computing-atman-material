fe4c32d52960daa966abdb970ad702854d2f8df9
atman-33 <gpbjk0304@gmail.com>
2023-07-12 22:45:09 +0900

    add breadcrumb

--- apps/client/src/app/blog/blog.module.ts
+++ apps/client/src/app/blog/blog.module.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule, Routes } from '@angular/router';
+import { BreadcrumbModule } from 'xng-breadcrumb';
 import { BlogComponent } from './blog.component';
 import { PostComponent } from './posts/post/post.component';
 import { PostModule } from './posts/post/post.module';
@@ -12,8 +14,19 @@ const routes: Routes = [
     path: 'blog', component: BlogComponent,
     children: [
       { path: '', redirectTo: 'posts', pathMatch: 'full' },
-      { path: 'posts', component: PostsComponent, title: 'Blog list | Computing Atman' },
-      { path: 'posts/:name', component: PostComponent},
+      {
+        path: 'posts',
+        component: PostsComponent,
+        title: 'Blog list | Computing Atman',
+        data: { breadcrumb: { skip: true } },
+      },
+      {
+        path: 'posts/:name',
+        component: PostComponent,
+        data: {
+          breadcrumb: (resolvedName: string) => `Viewing ${resolvedName} now!`
+        }
+      },
     ]
   }
 ];
@@ -26,7 +39,9 @@ const routes: Routes = [
     RouterModule.forChild(routes),
     CommonModule,
     PostsModule,
-    PostModule
+    PostModule,
+    BreadcrumbModule,
+    MatIconModule
   ],
 })
 export class BlogModule { }
---
+++ docs/breadcrumb.md
@@ -0,0 +1,10 @@
+## links
+https://betterprogramming.pub/add-breadcrumbs-to-your-angular-app-in-just-5-minutes-3119e376e901  
+
+https://udayvunnam.github.io/xng-breadcrumb/#/  
+
+## install npm i xng-breadcrumb
+```
+npm i xng-breadcrumb
+```
+
